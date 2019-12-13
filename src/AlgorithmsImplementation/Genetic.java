b97925ce56b42308c6478d57431b65042e81d2f2
karim-mohamed <k.mohamed3@hotmail.com>
2019-12-13 23:13:48 +0200

    Adding Genetic Algorithm and Modifying KBeamSearch Algorithm

--- src/AlgorithmsImplementation/Genetic.java
+++ src/AlgorithmsImplementation/Genetic.java
@@ -1,4 +1,93 @@
 package AlgorithmsImplementation;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class Genetic {
+    private int numOfExpandedNodes = 0;
+    private double totalRunTime = -1;
+    private int steps = -1;
+
+    public int[] solve(int n, int[] givenState, int populationSize, double mutationProbability, int numOfGenerations) {
+        double startTime = System.currentTimeMillis();
+        populationSize -= (populationSize % 2); // each one should get a mate.
+        int[][] population = generatePopulation(n, populationSize);
+        population[0] = givenState;
+        int maxFitness = getMaxFitness(n);
+        for (int x = 0; x < numOfGenerations; x++) {
+            population = getSelectedPopulation(population);
+            population = handleCrossovers(population, n);
+            for (int i = 0; i < populationSize; i++) {
+                if (getFitness(population[i]) == maxFitness) {
+                    double endTime = System.currentTimeMillis();
+                    totalRunTime = endTime - startTime;
+                    steps = x;
+                    return population[i];
+                }
+                population[i] = tryToMutate(population[i], mutationProbability);
+                if (getFitness(population[i]) == maxFitness)
+                    return population[i];
+            }
+        }
+        return null;
+    }
+
+    private int[][] handleCrossovers(int[][] population, int n) {
+        for (int i = 0; i < population.length; i += 2) {
+            int crossoverPos = (int) (Math.random() * n);
+            for (int j = 0; j < crossoverPos; j++) {
+                int tmp = population[i][j];
+                population[i][j] = population[i + 1][j];
+                population[i + 1][j] = tmp;
+            }
+
+        }
+        return population;
+    }
+
+    private int[][] getSelectedPopulation(int[][] population) {
+        Arrays.sort(population, Comparator.comparingInt(this::getFitness));
+        return population;
+    }
+
+    private int[] tryToMutate(int[] r, double mutationProbability) {
+        if (satisfyProb(mutationProbability))
+            r[(int) (Math.random() * r.length)] = (int) (Math.random() * r.length);
+        return r;
+    }
+
+    private boolean satisfyProb(double prob) {
+        return prob >= Math.random();
+    }
+
+    private int getFitness(int[] r) {
+        return getMaxFitness(r.length) - SolverUtils.getHeuristicCost(r);
+    }
+
+    private int getMaxFitness(int n) {
+        return n * (n - 1) / 2;
+    }
+
+    private int[] generateChromosome(int n) {
+        return SolverUtils.generateRandomState(n);
+    }
+
+    private int[][] generatePopulation(int n, int populationSize) {
+        int[][] population = new int[populationSize][];
+        for (int i = 1; i < populationSize; i++)
+            population[i] = generateChromosome(n);
+        return population;
+    }
+
+    public int getNumOfExpandedNodes() {
+        return numOfExpandedNodes;
+    }
+
+    public double getTotalRunTime() {
+        return totalRunTime;
+    }
+
+    public int getSteps() {
+        return steps;
+    }
 }
--- src/AlgorithmsImplementation/KBeamSearch.java
+++ src/AlgorithmsImplementation/KBeamSearch.java
@@ -4,28 +4,37 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public class KBeamSearch {
-    public int[] solve(int n,int[] givenState, int maxNumOfIterations, int k) {
+    private int numOfExpandedNodes = 0;
+    private double totalRunTime = -1;
+    private int steps = -1;
+
+    public int[] solve(int n, int[] givenState, int maxNumOfIterations, int k) {
+        double startTime = System.currentTimeMillis();
         int[][] states = new int[k][];
         states[0] = givenState;
-
         for (int i = 1; i < k; i++)
             states[i] = SolverUtils.generateRandomState(n);
 
         for (int x = 0; x < maxNumOfIterations; x++) {
-            int[][] newStates = new int[n*k][]; // each state of the k states is a move for a single queen from the n queens by one step
+            int[][] newStates = new int[n * k][]; // each state of the k states is a move for a single queen from the n queens by one step
             for (int i = 0; i < k; i++) {
                 int costToBeat = SolverUtils.getHeuristicCost(states[i]);
 
-                if (costToBeat == 0)
+                if (costToBeat == 0) {
+                    double endTime = System.currentTimeMillis();
+                    totalRunTime = endTime - startTime;
+                    steps = x;
                     return states[i];
+                }
 
                 for (int col = 0; col < n; col++) {
-                    newStates[i*n + col] = makeMove(states[i], col, costToBeat);
+                    newStates[i * n + col] = makeMove(states[i], col, costToBeat);
 
-                    if (newStates[i*n + col] == null)
-                        newStates[i*n + col] = SolverUtils.generateRandomState(n);
+                    if (newStates[i * n + col] == null)
+                        newStates[i * n + col] = SolverUtils.generateRandomState(n);
                 }
             }
+            numOfExpandedNodes += k;
             Arrays.sort(newStates, Comparator.comparingInt(SolverUtils::getHeuristicCost));
             states = Arrays.copyOfRange(newStates, 0, k);
         }
@@ -50,6 +59,18 @@ public class KBeamSearch {
             r[col] = tmpRow;
         }
         return null;
+    }
+
+    public int getNumOfExpandedNodes() {
+        return numOfExpandedNodes;
+    }
+
+    public double getTotalRunTime() {
+        return totalRunTime;
+    }
+
+    public int getSteps() {
+        return steps;
     }
 
 }
